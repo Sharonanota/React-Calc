44ea233699032477af3a00d3fb3fa744096bb63f
Sharonanota <matakwamatakwa@gmail.com>
2023-07-17 19:38:19 +0300

    calculate

---
+++ src/components/Screen.js
@@ -0,0 +1,18 @@
+import { Textfit } from "react-textfit";
+import "./Screen.css";
+
+
+
+
+
+export const Screen = ({ value }) => {
+  
+  
+  return (
+    <Textfit className="screen" mode="single" max={70}>
+      {value}
+    </Textfit>
+  );
+};
+
+export default Screen;
